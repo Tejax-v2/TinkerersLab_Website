9ae48b256548aaedee9e5a0a549f776723f9fe91
ojhapiyush <ojhapiyush063@gmail.com>
2023-07-28 19:29:35 +0530

    Fixed Navbar

---
+++ js/navbar.js
@@ -0,0 +1,21 @@
+// Function to handle smooth scrolling when a navbar link is clicked
+function smoothScroll(targetId) {
+    const target = document.querySelector(targetId);
+    const offsetTop = target.getBoundingClientRect().top;
+    const navbarHeight = document.querySelector('.navbar-collapse').offsetHeight;
+    const scrollTarget = offsetTop + window.scrollY - navbarHeight;
+
+    window.scroll({
+      top: scrollTarget,
+      behavior: 'smooth'
+    });
+  }
+
+  // Attach click event listeners to navbar links
+  document.querySelectorAll('.navbar-collapse    a').forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const targetId = link.getAttribute('href');
+      smoothScroll(targetId);
+    });
+  });
